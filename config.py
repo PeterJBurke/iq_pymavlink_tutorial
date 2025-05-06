f01ae9660e616b31180797bb3cd36958a2abfa30
Peter Burke <peter.burke.john@gmail.com>
2025-05-05 20:24:01 -0700

    Update MAVLink connection port to 5768

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # MAVLink connection settings
-MAVLINK_IP = '192.168.1.229'  # Default IP address
-MAVLINK_PORT = 14559          # Default port
+MAVLINK_IP = '192.168.193.46'  # Default IP address
+MAVLINK_PORT = 5678          # Default port
 
 # Connection string format
-CONNECTION_STRING = f'{MAVLINK_IP}:{MAVLINK_PORT}'
+CONNECTION_STRING = f'tcp:{MAVLINK_IP}:{MAVLINK_PORT}'
 
 # You can add more configuration settings here as needed
 # For example:
